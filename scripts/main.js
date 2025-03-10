c8e95aa3887b01a28174ee7d02a9c8703424af56
Davidpm02 <davidpamu02@gmail.com>
2025-03-10 22:33:16 +0100

    Cambios en estructura HTML principal.

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Inicializamos las variables principales
+let btnAddTask = document.getElementById('btn_todo_header')
+
+
+// Evento de agregación de nueva tarea
+btnAddTask.addEventListener('click', function () {
+    // Apertura del modal de registro de tarea
+})
